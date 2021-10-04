fba746e4e4d31eef7d906a3d32e736db07f58287
WP2-DickyMaulana <91734077+WP2-DickyMaulana@users.noreply.github.com>
2021-10-04 17:06:04 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalan<h1>";
+		echo "Nama saya Dicky Maulana
+		Saya tinggal di daerah cibarusa
+		olahraga yang saya sukai adalah
+		Futsal";
+	}
+}
